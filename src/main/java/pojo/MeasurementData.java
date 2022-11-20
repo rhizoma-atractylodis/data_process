31abd125ce0a47a969d406fa8428287c7be4ba25
atractylodis <atractylodis@gmail.com>
2022-11-20 11:23:29 +0800

    test db

--- src/main/java/pojo/MeasurementData.java
+++ src/main/java/pojo/MeasurementData.java
@@ -1,11 +1,13 @@
 package pojo;
 
 import com.influxdb.annotations.Column;
+import com.influxdb.annotations.Measurement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 
+@Measurement(name = "base")
 public class MeasurementData {
     @Column(name = "host", tag = true)
     private String host;
@@ -37,6 +39,129 @@ public class MeasurementData {
 
     public String getType() {
         return "unknown";
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getDestA() {
+        return destA;
+    }
+
+    public void setDestA(String destA) {
+        this.destA = destA;
+    }
+
+    public String getDestB() {
+        return destB;
+    }
+
+    public void setDestB(String destB) {
+        this.destB = destB;
+    }
+
+    public String getDestC() {
+        return destC;
+    }
+
+    public void setDestC(String destC) {
+        this.destC = destC;
+    }
+
+    public String getDestD() {
+        return destD;
+    }
+
+    public void setDestD(String destD) {
+        this.destD = destD;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+    public String getMeasurementPrefix() {
+        return measurementPrefix;
+    }
+
+    public void setMeasurementPrefix(String measurementPrefix) {
+        this.measurementPrefix = measurementPrefix;
+    }
+
+    public double getRtt() {
+        return rtt;
+    }
+
+    public void setRtt(double rtt) {
+        this.rtt = rtt;
+    }
+
+    public Instant getTime() {
+        return time;
+    }
+
+    public void setTime(Instant time) {
+        this.time = time;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "MeasurementData{" +
+                "host='" + host + '\'' +
+                ", destA='" + destA + '\'' +
+                ", destB='" + destB + '\'' +
+                ", destC='" + destC + '\'' +
+                ", destD='" + destD + '\'' +
+                ", protocol='" + protocol + '\'' +
+                ", round=" + round +
+                ", measurementPrefix='" + measurementPrefix + '\'' +
+                ", rtt=" + rtt +
+                ", time=" + time +
+                ", country='" + country + '\'' +
+                ", region='" + region + '\'' +
+                ", city='" + city + '\'' +
+                '}';
     }
 
     public void setByData(MeasurementData data) {
